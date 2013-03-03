1078d057d92aa87b4d5c1b9112b73f5806a69198
xemdetia <xemdetia@808inorganic.com>
2013-03-03 15:52:54 -0500

    lined up the usersessions table with the model in Sessions.py

--- setup/db/sqlite/create_usersessions_table.sql
+++ setup/db/sqlite/create_usersessions_table.sql
@@ -1,9 +1,12 @@
 
-
 CREATE TABLE usersessions (
 
-       -- 
+       -- session_id's are UUID's as integers
+       session_id BIGINT PRIMARY KEY,
        
+       -- user_id's line up with the users table
+       user_id BIGINT,
+              
        -- These two values encode easily the created time of the
        -- session and the time they were last seen. At creation these
        -- should be the same. When lastseen and created differ by a
@@ -12,4 +15,3 @@ CREATE TABLE usersessions (
        created DATETIME,
        lastseen DATETIME
 );
-       
