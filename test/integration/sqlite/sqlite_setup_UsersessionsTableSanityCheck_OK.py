d1e433337abb0ad0a5235564ea459b46fcdeb0bd
xemdetia <xemdetia@808inorganic.com>
2013-03-03 15:53:40 -0500

    Started to write an integration test case

---
+++ test/integration/sqlite/sqlite_setup_UsersessionsTableSanityCheck_OK.py
@@ -0,0 +1,12 @@
+
+from dbsetup import sqlite_setup
+import sqlite3
+import unittest
+
+class UsersessionsTableSanityCheck(unittest.TestCase):
+    
+    def test_
+
+
+if __name__ == "__main__":
+    unittest.main()
